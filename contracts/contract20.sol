d4ed208b34b43b5f610bf6e139333c83bebcaa93
Kainy <iartkhay@gmail.com>
2022-03-16 23:54:53 +0100

    task3 first commit

---
+++ contracts/contract20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract first as ERC721  {
+    using Counters for Counters.counter;
+
+    constructor () ERC721, Ownable {}
+    
+} 
